f62a513c07a720f29b1a8426379b85a3619fb8ad
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-04-22 02:28:57 +0200

    4. Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_4_usa\
+take 3 arguments: mysql username, mysql password and database name\
+Results must be sorted in ascending order by cities.id\
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
