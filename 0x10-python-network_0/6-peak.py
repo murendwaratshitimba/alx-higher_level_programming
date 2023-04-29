aca51d20037b648f2570ac1ee79cdf6b66de3e9e
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-04-29 03:03:57 +0200

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,11 +12,11 @@ def find_peak(list_of_integers):
         -> Your algorithm must have the lowest complexity
     """
 
-    
+
     # if there is no list of integers return None
     if list_of_integers == []:
         return None
-    
+
     length = len(list_of_integers)
 
     start_index, end_index = 0, length - 1
@@ -30,9 +30,9 @@ def find_peak(list_of_integers):
            list_of_integers[middle_index - 1] \
            and list_of_integers[middle_index] > \
            list_of_integers[middle_index + 1]:
-            
+
             return list_of_integers[middle_index]
-        
+
         if list_of_integers[middle_index - 1]\
             > list_of_integers[middle_index + 1]:
             end_index = middle_index
