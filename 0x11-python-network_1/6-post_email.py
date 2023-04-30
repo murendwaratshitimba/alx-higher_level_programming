45560e2cbc1a77b9a4d6e09e029744e749c0fc97
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-01 00:40:56 +0200

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,4 +11,5 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    print(requests.post(argv[1], data={"email": argv[2]}))
+    value = {"email": argv[2]}
+    print(requests.post(argv[1], data=value))
