d0bfdef7a47e2bc8590e175ab3f44e50f8701fe2
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-23 20:07:51 +0200

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
