5767e540f453db799fad4807438e65b4b8f29fc5
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-23 20:40:15 +0200

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that display the status code of a GET request
+
+const request = require('request');
+
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
+  }
+
+  console.log('code:', response && response.statusCode);
+});
