5825133bba8ca2e2cfa3cca099f51d5947ff3292
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-23 20:48:05 +0200

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,18 @@
 #!/usr/bin/node
 
-// script that prints the title of a Star Wars movie where the episode number matches a given integer
+/* script that prints the title of a Star Wars movie where
+the episode number matches a given integer */
 
 const request = require('request');
 const episode = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(API_URL + episode, function (error, response, body) {
-
   if (error) {
-
     console.log(error);
-
   } else if (response.statusCode === 200) {
-
     console.log(JSON.parse(body).title);
-
   } else {
-    
     console.log('Error code: ' + response.statusCode);
   }
 });
