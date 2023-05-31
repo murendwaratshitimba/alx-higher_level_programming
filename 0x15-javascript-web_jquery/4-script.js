e3c18c4db021d549d44faa6cdc34d53c028fdd88
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-31 23:48:54 +0200

    4. Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Write a JavaScript script that toggles the class of the <header> element 
+ * when the user clicks on the tag DIV#toggle_header
+ */
+
+$('div#toggle_header').click(function () {
+
+    $('header').ToggleClass('red');
+
+  });
